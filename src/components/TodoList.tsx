9943dbd5115b2de5e7406346ffb3544446fd5929
bitbytejoy <bitbytejoy@gmail.com>
2022-12-20 11:37:08 +0100

    feat: Kanbanstyle with filters

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,7 @@
 import Todo from "../types/Todo";
 import TodoItem from "./TodoItem";
 import TodoListTodo from "../types/TodoListTodo";
+import TodoStatus from "../types/TodoStatus";
 
 export default function TodoList ({
   todos,
@@ -17,20 +18,26 @@ export default function TodoList ({
   onDelete: (id: string) => void,
   onAdvance: (todo: Todo) => void
 }) {
+  const open = todos.filter(todo => todo.status === TodoStatus.OPEN);
+  const inProgress = todos.filter(
+    todo => todo.status === TodoStatus.IN_PROGRESS
+  );
+  const done = todos.filter(todo => todo.status === TodoStatus.DONE);
+
+  const columns = [
+    { title: "Open", todos: open },
+    { title: "In Progress", todos: inProgress },
+    { title: "Done", todos: done },
+  ];
+
   return (
     <div className={"TodoList"}>
-      {todos.length
-        ? [
-          {className: "open", title: "Open"},
-          {className: "in_progress", title: "In Progress"},
-          {className: "done", title: "Done"}
-        ].map(({className: status, title}) => (
-          <div key={status} className={`TodoListColumn show-${status}`}>
-            <header>
-              <h3>{title}</h3>
-            </header>
+      {columns.map(({title, todos}) => (
+        <div key={title} className={"TodoListColumn"}>
+          <h3>{title}</h3>
+          <div>
             {todos.map(todo => (
-              <div key={todo.id} className={`TodoItemWrap ${todo.status}`}>
+              <div key={todo.id} className={"TodoItemWrap"}>
                 <TodoItem
                   todo={todo}
                   onChange={onChange}
@@ -43,11 +50,8 @@ export default function TodoList ({
               </div>
             ))}
           </div>
-        ))  : (
-          <div className={"TodoListEmpty"}>
-            <p>No todos found</p>
-          </div>
-        )}
+        </div>
+      ))}
     </div>
   );
 }
